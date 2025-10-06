e6104feb3b0147546969e6b5c28ccfcf1aafd18a
gruberse <gruber.se@gmail.com>
2025-10-06 22:03:26 +0200

    fix

--- app/models/solver/ga/survivor_selection/elitists/obj.py
+++ app/models/solver/ga/survivor_selection/elitists/obj.py
@@ -7,5 +7,5 @@ from app.models.solver.ga.survivor_selection.obj import SurvivorSelection
 
 class ElitistsSelection(ElitistsSelectionBase, SurvivorSelection):
     def select_survivors(self, population_size: int, individuals: List[Individual]) -> List[Individual]:
-        individuals.sort(key=lambda individual: individual.fitness_list[0].get_estimated_or_actual_fitness())
+        individuals.sort(key=lambda individual: individual.fitness_list[0].get_estimated_or_actual_fitness(), reverse=True)
         return individuals[:population_size]
