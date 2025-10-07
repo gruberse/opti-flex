a5d224aae0b4e54330dafa6bf71af1de3aaedda7
gruberse <gruber.se@gmail.com>
2025-10-07 11:04:10 +0200

    added tests for fitness eval

--- app/models/solver/ga/fitness_evaluation/combined/obj.py
+++ app/models/solver/ga/fitness_evaluation/combined/obj.py
@@ -1,13 +1,43 @@
 from typing import List
 
+import numpy as np
+
 from app.models.individual.obj import Individual
+from app.models.objective.tsp.obj import TravelingSalesmanProblemObjective
 from app.models.population.obj import Population
 from app.models.problem.obj import Problem
+from app.models.problem.tsp.obj import TravelingSalesmanProblem
 from app.models.solver.ga.fitness_evaluation.combined.base import CombinedEvaluationBase
 from app.models.solver.ga.fitness_evaluation.obj import FitnessEvaluation
 
 
 class CombinedEvaluation(CombinedEvaluationBase, FitnessEvaluation):
     def evaluate_individuals(self, problem: Problem, parents: List[Individual], offspring: List[Individual]) -> List[Individual]:
-        combined_individuals = parents + offspring
-        return problem.evaluate_individuals(combined_individuals)
+        return problem.evaluate_individuals(parents + offspring)
+
+def test():
+    e = CombinedEvaluation()
+
+    problem = TravelingSalesmanProblem(
+        objectives=[
+            TravelingSalesmanProblemObjective(objective_id="test", matrix=np.array([
+                [0, 1, 2],
+                [3, 0, 4],
+                [5, 6, 0]
+            ]))
+        ]
+    )
+
+    parents = [
+        Individual(encoding=[0, 1, 2])
+    ]
+
+    offspring = [
+        Individual(encoding=[2, 1, 0])
+    ]
+
+    evaluated_individuals = e.evaluate_individuals(problem, parents, offspring)
+
+    assert len(evaluated_individuals) == 2
+    assert evaluated_individuals[0].encoding == [0, 1, 2]
+    assert evaluated_individuals[1].encoding == [2, 1, 0]
--- app/models/solver/ga/fitness_evaluation/elitists/obj.py
+++ app/models/solver/ga/fitness_evaluation/elitists/obj.py
@@ -1,16 +1,21 @@
 import random
 from typing import List
 
+import numpy as np
+
+from app.models.fitness.obj import Fitness
 from app.models.individual.obj import Individual
+from app.models.objective.tsp.obj import TravelingSalesmanProblemObjective
 from app.models.population.obj import Population
 from app.models.problem.obj import Problem
+from app.models.problem.tsp.obj import TravelingSalesmanProblem
 from app.models.solver.ga.fitness_evaluation.elitists.base import ElitistsEvaluationBase
 from app.models.solver.ga.fitness_evaluation.obj import FitnessEvaluation
 
 
 class ElitistsEvaluation(ElitistsEvaluationBase, FitnessEvaluation):
     def evaluate_individuals(self, problem: Problem, parents: List[Individual], offspring: List[Individual]) -> List[Individual]:
-        individuals = offspring
+        individuals = offspring.copy()
 
         temp = Population(population_id=-1, individuals=parents)
         non_dominated_individuals = temp.get_non_dominated_individuals()
@@ -24,3 +29,44 @@ class ElitistsEvaluation(ElitistsEvaluationBase, FitnessEvaluation):
             individuals = individuals + non_dominated_individuals
 
         return problem.evaluate_individuals(individuals)
+
+
+def test():
+    e = ElitistsEvaluation(replace_offspring=False)
+
+    problem = TravelingSalesmanProblem(
+        objectives=[
+            TravelingSalesmanProblemObjective(objective_id="Objective 1", matrix=np.array([
+                [0, 1, 2],
+                [3, 0, 4],
+                [5, 6, 0]
+            ]))
+        ]
+    )
+
+    parents = [
+        Individual(encoding=[0, 1, 2], fitness_list=[Fitness(objective_id='test', actual_fitness=100)]),
+        Individual(encoding=[1, 2, 0], fitness_list=[Fitness(objective_id='test', actual_fitness=50)])
+    ]
+
+    offspring = [
+        Individual(encoding=[2, 1, 0]),
+        Individual(encoding=[0, 2, 1])
+    ]
+
+    random.seed(1)
+
+    evaluated_individuals = e.evaluate_individuals(problem, parents, offspring)
+
+    assert len(evaluated_individuals) == 3
+    assert evaluated_individuals[0].encoding == [2, 1, 0]
+    assert evaluated_individuals[1].encoding == [0, 2, 1]
+    assert evaluated_individuals[2].encoding == [0, 1, 2]
+
+    e = ElitistsEvaluation(replace_offspring=True)
+
+    evaluated_individuals = e.evaluate_individuals(problem, parents, offspring)
+
+    assert len(evaluated_individuals) == 2
+    assert evaluated_individuals[0].encoding == [0, 1, 2]
+    assert evaluated_individuals[1].encoding == [0, 2, 1]
